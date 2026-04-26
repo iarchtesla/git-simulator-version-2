3e83aba86869260596f7e74eda27afa3f2687423
Islam Hamdi <archtesla.engineer@gmail.com>
2026-04-27 01:44:32 +0300

    Update GitSimulator.h

--- GitSimulator.h
+++ GitSimulator.h
@@ -1,45 +1,32 @@
 #ifndef GITSIMULATOR_H
 #define GITSIMULATOR_H
 
-#include <list>     
-#include <stack>     
-#include <array>   
+#include <list>
+#include <stack>
+#include <array>
 #include <string>
 #include "Commit.h"
 
 class GitSimulator {
 private:
-  
     std::list<Commit> commits;
-    
-   
     std::stack<Commit> stashStack;
-    
-   
     static const size_t RECENT_SIZE = 3;
     std::array<Commit, RECENT_SIZE> recent;
     int nextId;
-    
+
     void updateRecentArray();
-    
+
 public:
     GitSimulator();
-
-
-    void addCommit(const std::string& msg);        
-    void showAllCommits() const;                     
-    bool searchCommit(const std::string& keyword) const; 
-    bool deleteLastCommit();                         
-    bool modifyLastCommit(const std::string& newMsg); 
-    
-   
-    void stash();                                   
-    void stashPop();                               
-    
- 
+    void addCommit(const std::string& msg);
+    void showAllCommits() const;
+    bool searchCommit(const std::string& keyword) const;
+    bool deleteLastCommit();
+    bool modifyLastCommit(const std::string& newMsg);
+    void stash();
+    void stashPop();
     void showRecentCommits() const;
-    
-   
     void explainDS() const;
 };
 
