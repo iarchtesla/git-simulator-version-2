2f740115e18b983f292ee1f45f4e0d46bf005764
Islam Hamdi <archtesla.engineer@gmail.com>
2026-04-27 01:41:51 +0300

    Update Commit.cpp

--- Commit.cpp
+++ Commit.cpp
@@ -3,11 +3,13 @@
 #include <sstream>
 #include <iomanip>
 
+Commit::Commit() : id(-1), message(""), timestamp("") {}
+
 Commit::Commit(int id, const std::string& msg) : id(id), message(msg) {
     std::time_t now = std::time(nullptr);
     std::tm* local = std::localtime(&now);
     std::ostringstream oss;
-    oss << std::put_time(local, "%H:%M:%S"); 
+    oss << std::put_time(local, "%H:%M:%S");
     timestamp = oss.str();
 }
 
