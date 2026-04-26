aa28c6d56a73b0ecdc23bd0664fc868c40b554cf
Islam Hamdi <archtesla.engineer@gmail.com>
2026-04-26 20:30:14 +0300

    Create Commit.cpp

---
+++ Commit.cpp
@@ -0,0 +1,16 @@
+#include "Commit.h"
+#include <ctime>
+#include <sstream>
+#include <iomanip>
+
+Commit::Commit(int id, const std::string& msg) : id(id), message(msg) {
+    std::time_t now = std::time(nullptr);
+    std::tm* local = std::localtime(&now);
+    std::ostringstream oss;
+    oss << std::put_time(local, "%H:%M:%S"); 
+    timestamp = oss.str();
+}
+
+void Commit::print() const {
+    std::cout << "[Commit " << id << "] " << message << " @ " << timestamp << "\n";
+}
